e3934d2333d3efed3a423fcf9cfa25e8373ee100
Deem3 <erdemee.t@gmail.com>
2023-06-13 18:06:15 +0800

    feat: EES-57 Completed header task

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,44 +1,58 @@
-// style components
-import { Box, Flex, Button, useColorMode } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  Image,
+  Text,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+} from "@chakra-ui/react";
+import { ChevronDownIcon } from "@chakra-ui/icons";
 
-// react
-import { Link } from "react-router-dom";
+enum Pro {
+  name = "С.Нямхүү",
+  position = "Эрхлэгч",
+}
 
 export default function Navbar() {
   return (
     <Flex
-      w="100vw"
-      h="3rem"
-      justify="space-evenly"
+      w="100%"
+      border="1px solid #f2f2f2"
+      justify="space-between"
       align="center"
-      bg={{ base: "red.600", md: "blue.500" }}
+      px="2rem"
     >
-      <Box>
-        <Link to="/">Home</Link>
-      </Box>
-      <Box>
-        <Link to="/about">About</Link>
-      </Box>
-      <Box>
-        <Link to="/contact">Contact</Link>
-      </Box>
-      <Example />
+      <Flex justify="center" align="center">
+        <Image src="src/assets/erin-logo.png" w="4rem" h="4rem" />
+        <Text fontWeight="extrabold" fontSize="24px" ml="1rem">
+          Erin Systems
+        </Text>
+      </Flex>
+      <Flex align="center">
+        <Box w="5rem">
+          <Text noOfLines={1}>{Pro.name}</Text>
+          <Text color="#4D4D4D">{Pro.position}</Text>
+        </Box>
+        <Menu>
+          <MenuButton>
+            <ChevronDownIcon w="4.5rem" h="4.5rem" />
+          </MenuButton>
+          <Flex justify="center" align="center">
+            <MenuList
+              border="none"
+              shadow="2xl"
+              bg="white"
+              boxShadow="1px 11px 8px -1px rgba(74,62,62,0.75);"
+              minW="0"
+              w="5rem"
+            >
+              <MenuItem bg="white">Гарах</MenuItem>
+            </MenuList>
+          </Flex>
+        </Menu>
+      </Flex>
     </Flex>
-  );
-}
-
-enum Theme {
-  Light = "light",
-  Dark = "dark",
-}
-
-function Example() {
-  const { colorMode, toggleColorMode } = useColorMode();
-  return (
-    <header>
-      <Button onClick={toggleColorMode}>
-        Toggle {colorMode === Theme.Light ? Theme.Dark : Theme.Light}
-      </Button>
-    </header>
   );
 }
