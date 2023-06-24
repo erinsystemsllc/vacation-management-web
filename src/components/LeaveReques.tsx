2a8cfee89c9bf65e09e3ab54781974256487521f
khulan11 <ehulan62@gmail.com>
2023-06-24 18:32:45 +0800

    feat: EES-43 Vacation request web

--- src/components/LeaveReques.tsx
+++ src/components/LeaveReques.tsx
@@ -1,10 +1,4 @@
 import {
-  ArrowBackIcon,
-  ArrowDownIcon,
-  ArrowUpDownIcon,
-  ChevronDownIcon,
-} from "@chakra-ui/icons";
-import {
   Modal,
   ModalOverlay,
   ModalContent,
@@ -16,14 +10,6 @@ import {
   Input,
   FormLabel,
   FormControl,
-  Menu,
-  MenuButton,
-  MenuList,
-  MenuItem,
-  MenuItemOption,
-  MenuGroup,
-  MenuOptionGroup,
-  MenuDivider,
   Select,
   NumberInput,
   NumberInputField,
@@ -31,54 +17,89 @@ import {
   NumberIncrementStepper,
   NumberDecrementStepper,
 } from "@chakra-ui/react";
-
+import { useQuery } from "@tanstack/react-query";
 import { useDisclosure } from "@chakra-ui/react";
 
+type Manager = {
+  firsName: string;
+  lastName: string;
+}
+    
 export default function LeaveReques() {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  
+  const {data, isFetching} = useQuery({
+    queryKey: ["leaveType"],
+    queryFn: async () => {
+      const response = await fetch("http://localhost:8000/api/vacation/leaveType")
+      const data = response.json()
+      return data;
+    }
+  })
+
+  const {data: managers} = useQuery({
+    queryKey: ["managers"],
+    queryFn: async () => {
+      const response = await fetch("http://localhost:8000/api/vacation/manager?role=Менежер")
+      const data = await response.json()
+      return data as Manager[];
+    }
+  })
+    
+  const getDisplayName = ({firsName, lastName}: Manager) => `${lastName[0] ?? ""}. ${firsName}`;
+
   return (
     <>
       <Button onClick={onOpen} bg="#6A994E" colorScheme="green">
         Чөлөө авах
       </Button>
-      <Modal isOpen={isOpen} onClose={onClose} isCentered size="3xl">
+      <Modal isOpen={isOpen} onClose={onClose} isCentered size="xl">
         <ModalOverlay />
         <ModalContent>
-          <ModalHeader fontSize="2xl">Чөлөөний хүсэлт</ModalHeader>
+          <ModalHeader  fontSize="2xl">Чөлөөний хүсэлт</ModalHeader>
           <ModalCloseButton />
           <ModalBody>
-            <FormControl
+            <FormControl 
               display="grid"
               gridTemplateColumns="50% 50%"
               alignItems="center"
+              
             >
-              <FormLabel fontSize="xl">Чөлөөний төрөл:</FormLabel>
-
-              <Select variant="outline" size="lg">
-                <option value="option1">Option 1</option>
-                <option value="option2">Option 2</option>
-                <option value="option3">Option 3</option>
+              <FormLabel fontSize="lg">Чөлөөний төрөл:</FormLabel>
+              
+              <Select variant="outline" size="md">
+              <option>Чөлөөний төрөл оруулна уу! </option>
+                {
+                  data?.map((type: string)=>
+                  {
+                    return(
+                      <>
+                        <option value={type}>{type}</option>
+                      </>
+                    )
+                  })
+                }
               </Select>
             </FormControl>
 
             <FormControl
-              my="1.5rem"
+              my="1.25rem"
               display="grid"
               gridTemplateColumns="50% 50%"
               alignItems="center"
             >
-              <FormLabel fontSize="xl">Чөлөөний авах огноо:</FormLabel>
-              <Input type="date" size="lg" />
+              <FormLabel fontSize="lg">Чөлөө авах огноо:</FormLabel>
+              <Input type="date" size="md" />
             </FormControl>
 
             <FormControl
-              my="1.5rem"
+              my="1.25rem"
               display="grid"
               gridTemplateColumns="50% 50%"
               alignItems="center"
             >
-              <FormLabel fontSize="xl">Чөлөө авах цаг:</FormLabel>
-              <NumberInput defaultValue={0} min={0} max={20}>
+              <FormLabel fontSize="lg">Чөлөө авах цаг:</FormLabel>
+              <NumberInput defaultValue={0} min={0} max={20} size="md">
                 <NumberInputField />
                 <NumberInputStepper>
                   <NumberIncrementStepper />
@@ -86,15 +107,28 @@ export default function LeaveReques() {
                 </NumberInputStepper>
               </NumberInput>
             </FormControl>
-
-            <Input my="2rem" placeholder="Өдрөө оруулна уу" isRequired />
+            <FormControl
+              display="grid"
+              gridTemplateColumns="50% 50%"
+              alignItems="center"
+            >
+              <FormLabel fontSize="lg">Чөлөөний хүсэлт илгээх менежер:</FormLabel>
+              <Select variant="outline" size="md">
+              <option>Менежер сонгоно уу! </option>
+                {
+                  managers?.map((manager, index) => (
+                    <option key={index} value={manager.lastName}>{getDisplayName(manager)}</option>
+                  ))
+                }
+              </Select>
+            </FormControl>
           </ModalBody>
 
           <ModalFooter display="flex" justifyContent="center">
-            <Button colorScheme="red" mr={3} onClick={onClose}>
+            <Button colorScheme="red" mr={3} onClick={onClose} size="md">
               Болих
             </Button>
-            <Button colorScheme="green" bg="#6A994E">
+            <Button colorScheme="green" bg="#6A994E" size="md">
               Хадгалах
             </Button>
           </ModalFooter>
