c1d4ff3dd0a334e778d932b01b54072b9eb487e2
khulan11 <ehulan62@gmail.com>
2023-06-28 16:49:04 +0800

    modified:   src/components/LeaveRequest.tsx 	modified:   src/hooks/useLoginForm.ts

--- src/components/LeaveRequest.tsx
+++ src/components/LeaveRequest.tsx
@@ -16,37 +16,31 @@ import {
   NumberInputStepper,
   NumberIncrementStepper,
   NumberDecrementStepper,
+  useToast,
+  useDisclosure,
 } from "@chakra-ui/react";
 import { useMutation, useQuery } from "@tanstack/react-query";
-import { useDisclosure } from "@chakra-ui/react";
-import { useState } from "react";
-import { useToast } from '@chakra-ui/react'
-
 type Manager = {
-  id: string,
-  firsName: string;
+  id: string;
+  firstName: string;
   lastName: string;
 };
 type LeaveType = {
-  typeName : string;
-  maxHour : number;
-  displayName:string ;
-
+  typeName: string;
+  maxHour: number;
+  displayName: string;
+};
+interface RequestForm {
+  // employeeId: string;
+  managerId: string;
+  type: string;
+  date: string;
+  hour: number;
 }
 export default function LeaveRequest() {
   const { isOpen, onOpen, onClose } = useDisclosure();
-  const [jsonData, setJsonData] = useState("");
-  const handleClick = () => {
+  const toast = useToast();
 
-    toast({
-      title: 'Account created.',
-      description: "We've created your account for you.",
-      status: 'success',
-      duration: 9000,
-      isClosable: true,
-})
-  }
-  const toast = useToast()
   const { data: leaveTypes } = useQuery({
     queryKey: ["leaveTypes"],
     queryFn: async () => {
@@ -64,60 +58,62 @@ export default function LeaveRequest() {
         "http://localhost:8000/api/user/approvedby?role=Менежер"
       );
       const data = await response.json();
-      console.log(data)
       return data as Manager[];
     },
   });
   const saveMutation = useMutation({
-    mutationFn: async (jsonData: saveReq) => {
-      const response = await fetch(
-        "http://localhost:8000/api/vacation/save", {
-          method: "POST",
-          body: JSON.stringify(jsonData),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
-      const data = await response.json();
-      console.log(data)
-      return data as Request[];
-    },onSuccess: () => {
+    mutationFn: (jsonData: RequestForm) =>
+      fetch("http://localhost:8000/api/vacation/save", {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(jsonData),
+      }),
+    onSuccess: () => {
+      toast({
+        title: "Aмжилттай хадгалагдлаа.",
+        description: "Чөлөөний хүсэлт илгээгдсэн.",
+        status: "success",
+        duration: 9000,
+        isClosable: true,
+      });
+      onClose();
+    },
+    onError: () => {
+      toast({
+        title: "Амжилтгүй.",
+        description: "Хүсэлт илгээгдсэнгүй.",
+        status: "error",
+        duration: 9000,
+        isClosable: true,
+      });
+    },
+  });
+  const getDisplayName = ({ firstName, lastName }: Manager) =>
+    `${lastName[0] ?? ""}. ${firstName}`;
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    const type = event.target[1].value;
+    const hour = parseInt(event.target[3].value);
+    const managerId = event.target[4].value;
+    // const token = sessionStorage.getItem("token");
+    const date = event.target[2].value;
+    const data = { type, date, hour, managerId };
+    if (hour === 0) {
+      // Display an error toast or alert indicating that the hour should be greater than zero
+      toast({
+        title: "Алдаа!",
+        description: "Чөлөө авах цагаа оруулна уу.",
+        status: "error",
+        duration: 9000,
+        isClosable: true,
+      });
+      return;
     }
-  })
-interface saveReq{
-  employeeId: string,
-  managerId: string,
-  type: string,
-  date: Date,
-  hour: number
-}  
-  const getDisplayName = ({ firsName, lastName }: Manager) =>
-    `${lastName[0] ?? ""}. ${firsName}`;
-
-    const handleFetch = (event: React.FormEvent) => {
-      event.preventDefault()
-      const d = new Date();
-      const year = d.getFullYear();
-      const date1 = d.getDate();
-      const m = d.getMonth() + 1;
-      const month = m < 10 ? "0" + m : m;
-      const hours = d.getHours();
-      const minutes = d.getMinutes();
-      const seconds = d.getSeconds();
-      const currentDate =year + "-" + month + "-" + date1 + "-" + hours + "-" + minutes + "-" + seconds;
-      const stringComp = event.target[2].value.localeCompare(currentDate);
-      const date = stringComp >= 0 ? event.target[2].value : null;
-      const type = event.target[1].value;
-      const hour = parseInt(event.target[3].value);
-      const managerId = event.target[4].value;
-       const token = sessionStorage.getItem('token')
-       const typeName = event.target[1].name
-      //  const requestedUserId = JSON.parse(token).id;
-      // saveMutation.mutate({type, date, hour,managerId})
-      console.log({type,hour, date, managerId})
-    }
-
+    saveMutation.mutate(data);
+  };
+  const currentDate = () => new Date().toISOString().slice(0, 10);
   return (
     <>
       <Button onClick={onOpen} bg="#6A994E" colorScheme="green">
@@ -125,8 +121,11 @@ interface saveReq{
       </Button>
       <Modal isOpen={isOpen} onClose={onClose} isCentered size="xl">
         <ModalOverlay />
-        <ModalContent as="form" onSubmit={(event: React.FormEvent)=>handleFetch(event)}>
-          <ModalHeader as="form" onSubmit={(event: React.FormEvent)=>handleFetch(event)}e="2xl">Чөлөөний хүсэлт</ModalHeader>
+        <ModalContent
+          as="form"
+          onSubmit={(event: React.FormEvent) => handleSubmit(event)}
+        >
+          <ModalHeader fontSize="2xl">Чөлөөний хүсэлт</ModalHeader>
           <ModalCloseButton />
           <ModalBody>
             <FormControl
@@ -135,14 +134,18 @@ interface saveReq{
               alignItems="center"
             >
               <FormLabel fontSize="lg">Чөлөөний төрөл:</FormLabel>
-              <Select variant="outline" size="md">
-                <option>Чөлөөний төрөл оруулна уу! </option>
-                {leaveTypes?.map((leaveType)=>{
-                  return(
+              <Select variant="outline" size="md" required>
+                <option value="" disabled selected hidden>
+                  Чөлөөний төрөл оруулна уу!
+                </option>
+                {leaveTypes?.map((leaveType) => {
+                  return (
                     <>
-                    <option value={`${leaveType.typeName}`}>{leaveType.displayName}</option>
+                      <option value={`${leaveType.typeName}`}>
+                        {leaveType.displayName}
+                      </option>
                     </>
-                  )
+                  );
                 })}
               </Select>
             </FormControl>
@@ -155,7 +158,9 @@ interface saveReq{
               <FormLabel fontSize="lg">Чөлөө авах огноо:</FormLabel>
               <Input
                 type="date"
+                defaultValue={currentDate()}
                 size="md"
+                min={currentDate()}
               />
             </FormControl>
             <FormControl
@@ -165,8 +170,8 @@ interface saveReq{
               alignItems="center"
             >
               <FormLabel fontSize="lg">Чөлөө авах цаг:</FormLabel>
-              <NumberInput defaultValue={0} min={0} max={20} size="md">
-                <NumberInputField/>
+              <NumberInput defaultValue={0} min={0} max={355} size="md">
+                <NumberInputField />
                 <NumberInputStepper>
                   <NumberIncrementStepper />
                   <NumberDecrementStepper />
@@ -181,8 +186,10 @@ interface saveReq{
               <FormLabel fontSize="lg">
                 Чөлөөний хүсэлт илгээх менежер:
               </FormLabel>
-              <Select variant="outline" size="md">
-                <option>Менежер сонгоно уу! </option>
+              <Select variant="outline" size="md" required>
+                <option value="" disabled selected hidden>
+                  Менежер сонгоно уу!
+                </option>
                 {managers?.map((manager, index) => (
                   <option key={index} value={manager.id}>
                     {getDisplayName(manager)}
@@ -195,9 +202,8 @@ interface saveReq{
             <Button colorScheme="red" mr={3} onClick={onClose} size="md">
               Болих
             </Button>
-            <Button colorScheme="green" bg="#6A994E" size="md" type="submit" onClick={()=>{onClose(), handleClick()}}>
+            <Button colorScheme="green" bg="#6A994E" size="md" type="submit">
               Хадгалах
-
             </Button>
           </ModalFooter>
         </ModalContent>
