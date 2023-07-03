1c59398d137a0cc086d86235e79a1955d1cf8d58
Deem3 <erdemee.t@gmail.com>
2023-07-03 17:00:43 +0800

    fix: :bug: removed left logics

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import { Spinner, Flex } from "@chakra-ui/react";
 
 import { Route, Routes, useNavigate, useLocation} from "react-router-dom";
 import { useEffect } from "react";
-import { useQueryClient } from "@tanstack/react-query";
 
+// components
 import { Box } from "@chakra-ui/react";
 import Topbar from "./components/Topbar";
 import Home from "./pages/Home";
@@ -13,23 +13,22 @@ import Sidebar from "./components/Sidebar";
 import Info from "./pages/Info";
 
 export default function App() {
-  const queryClient = useQueryClient();
   const navigate = useNavigate();
   const token = sessionStorage.getItem("token");
   const json = token ? JSON.parse(token) : null;
   const role = json?.role;
+
   useEffect(() => {
     if (token === null) {
-      queryClient.clear();
       navigate("/login");
     }
-  }, [token, navigate, queryClient]);
+  }, [token, navigate]);
 
   const { pathname } = useLocation();
 
   return (
     <Box w="100vw" h="100vh" overflow="hidden">
-      {pathname === "/login" ? null : token !== null ? null : <Topbar />}
+      {pathname === "/login" ? null : token === null ? null : <Topbar />}
       <Flex w="100%" h="100%">
         <Box>
           {pathname === "/login" ? null : token === null ? null : <Sidebar />}
@@ -38,7 +37,7 @@ export default function App() {
           <Route
             path="/"
             element={
-              token !== null ? (
+              token === null ? (
                 <Flex
                   w="100vw"
                   h="100vh"
@@ -85,7 +84,7 @@ export default function App() {
           /> <Route
           path="/history"
           element={
-              role !== 'Менежер'? (
+              role !== 'Менежер' ? (
               <Flex
                 w="100vw"
                 h="100vh"
